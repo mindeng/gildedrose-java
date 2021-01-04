1d152dc52b5ebf50063e44fe2b5aba9a0ef48440
dengmin <minotes.net@gmail.com>
2021-01-04 18:45:01 +0800

    move extracted method to Item

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,55 @@ public class Item {
         this.quality = quality;
     }
 
+    void extracted() {
+        if (!this.name.equals("Aged Brie")
+                && !this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (this.quality > 0) {
+                if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    this.quality = this.quality - 1;
+                }
+            }
+        } else {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1;
+
+                if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (this.sellIn < 11) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1;
+                        }
+                    }
+
+                    if (this.sellIn < 6) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+            this.sellIn = this.sellIn - 1;
+        }
+
+        if (this.sellIn < 0) {
+            if (!this.name.equals("Aged Brie")) {
+                if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (this.quality > 0) {
+                        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            this.quality = this.quality - 1;
+                        }
+                    }
+                } else this.quality = 0;
+            } else {
+                if (this.quality < 50) {
+                    this.quality = this.quality + 1;
+                }
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
