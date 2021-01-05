f2857b09de64dcadfff21f0c989cf8245fae8fee
dengmin <minotes.net@gmail.com>
2021-01-05 22:51:16 +0800

    simplify Item.updateQuality()

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -47,13 +47,8 @@ public class Item {
     }
 
     protected void updateQuality() {
-        if (!isAgedBrie()
-                && !isBackstage()) {
-            if (this.quality > 0) {
-                if (!isSulfuras()) {
-                    this.quality = this.quality - 1;
-                }
-            }
+        if (this.quality > 0) {
+            this.quality = this.quality - 1;
         }
     }
 
