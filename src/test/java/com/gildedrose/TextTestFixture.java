bbf2465937bd764ad115bcb2eeeb76603b3b142f
dengmin <minotes.net@gmail.com>
2021-01-05 21:20:10 +0800

    extract createSulfuras()

--- src/test/java/com/gildedrose/TextTestFixture.java
+++ src/test/java/com/gildedrose/TextTestFixture.java
@@ -14,18 +14,18 @@ public class TextTestFixture {
 
         printStream.println("OMGHAI!");
 
-        Item[] items = new Item[] {
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 Item.createAgedBrie(2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                createSulfuras(0, 80), //
+                createSulfuras(-1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
@@ -40,6 +40,10 @@ public class TextTestFixture {
         }
 
         return outputStream.toString();
+    }
+
+    private static Item createSulfuras(int sellIn, int quality) {
+        return new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
     }
 
 }
